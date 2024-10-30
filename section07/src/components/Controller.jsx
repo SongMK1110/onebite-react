40422276c2c2ba967c166fd06099b1c406483270
songminkyu <thdalsrb123@naver.com>
2024-10-30 14:07:48 +0900

    first commit

---
+++ section07/src/components/Controller.jsx
@@ -0,0 +1,52 @@
+const Controller = ({ onClickButton }) => {
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() => {
+            onClickButton(-1);
+          }}
+        >
+          -1
+        </button>
+        <button
+          onClick={() => {
+            onClickButton(-10);
+          }}
+        >
+          -10
+        </button>
+        <button
+          onClick={() => {
+            onClickButton(-100);
+          }}
+        >
+          -100
+        </button>
+        <button
+          onClick={() => {
+            onClickButton(+100);
+          }}
+        >
+          +100
+        </button>
+        <button
+          onClick={() => {
+            onClickButton(+10);
+          }}
+        >
+          +10
+        </button>
+        <button
+          onClick={() => {
+            onClickButton(+1);
+          }}
+        >
+          +1
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Controller;
